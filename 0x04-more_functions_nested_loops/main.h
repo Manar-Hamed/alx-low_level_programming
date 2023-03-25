c405c09e96a7439fa848a339e5dafa192bc158a1
Manar-Hamed <man.adel@nu.edu.eg>
2023-03-24 17:32:27 -0700

    2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -6,5 +6,6 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
