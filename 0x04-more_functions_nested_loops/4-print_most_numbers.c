1e7926fcde4f923674c694e3cc3206254981a4c3
Manar-Hamed <man.adel@nu.edu.eg>
2023-03-24 17:55:17 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints 0-9 with a new line, excludes 2 and 4
+* Description: Uses headers to link and a nested loops to achieve goal
+* Return: void. no return.
+*/
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
+	}
+	_putchar('\n');
+}
+
