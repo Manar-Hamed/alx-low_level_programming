421c59a0e454305d2edaba12211d587a29867632
Manar-Hamed <man.adel@nu.edu.eg>
2023-03-27 15:25:33 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
   * rev_string - Reverses a string
-  * s=> The string to be modified
+  * @s: The string to be modified
   *
   * Return: void
   */
