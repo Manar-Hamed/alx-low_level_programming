9dd4d5d5cb137fc017dc3723d5dc89e990e0de9e
Manar-Hamed <man.adel@nu.edu.eg>
2023-04-21 17:21:26 -0700

    0F

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,18 +1,19 @@
 #include "function_pointers.h"
 
 /**
-* int_index: search for an integer
+* int_index -  searching for an integer
 * @array: int array
 * @size: size array
 * @cmp: comparison of the function
 *
-* return: void
+* Return: void
 */
 int int_index(int *array, int size, int (*cmp)(int));
 {
 	int i = 0;
+
 	if (array && size & cmp)
-		while (i<size)
+		while (i < size)
 		{
 		if (cmp(array[i]))
 			return (i);
